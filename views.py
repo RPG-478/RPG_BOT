f6c52b58783a21b73299ac91055e7827445c04fd
RPG-478 <ucs250310@hs.city-saitama.ed.jp>
2025-10-25 08:57:30 +0900

    views.py を更新

--- views.py
+++ views.py
@@ -4,7 +4,6 @@ import random
 import asyncio
 import game
 from db import get_player, update_player, delete_player
-from death_system import handle_death_with_triggers
 
 # -------------------------
 # 名前入力View
