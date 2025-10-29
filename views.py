57271c8a618c16f1196349e1750b7c9a76a16d0e
RPG-478 <ucs250310@hs.city-saitama.ed.jp>
2025-10-30 08:22:41 +0900

    views.py を更新

--- views.py
+++ views.py
@@ -2520,8 +2520,8 @@ class BattleView(View):
                     self.player["defense"] = base_def + equipment_bonus["defense_bonus"]
                     print(f"[DEBUG] run - プレイヤーデータ最新化: HP={self.player['hp']}, DEF={base_def}+{equipment_bonus['defense_bonus']}={self.player['defense']}")
 
-                # 逃走確率（仮に進んだ距離がplayer["distance"]）
-                if random.randint(1, 100) <= 50:
+                # 逃走確率
+                if random.randint(1, 100) <= 20:
                     # 逃走成功 - HPを保存
                     await db.update_player(interaction.user.id, hp=self.player["hp"])
                     text = "🏃‍♂️ うまく逃げ切れた！\n『戦っとけば良かったかな――。』"
