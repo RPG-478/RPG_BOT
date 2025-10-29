718f288222e2418e936fb1ca14ac36f030031f55
npprtkt-ship-it <npprtkt@gmail.com>
2025-10-29 22:45:32 +0900

    Update game.py
    スキル調整

--- game.py
+++ game.py
@@ -2359,7 +2359,7 @@ SKILLS_DATABASE = {
         "id": "体当たり",
         "name": "体当たり",
         "type": "attack",
-        "mp_cost": 10,
+        "mp_cost": 3,
         "power": 1.2,
         "description": "基本的な体当たり攻撃。威力1.2倍。",
         "unlock_distance": 0
@@ -2368,7 +2368,7 @@ SKILLS_DATABASE = {
         "id": "小火球",
         "name": "小火球",
         "type": "attack",
-        "mp_cost": 15,
+        "mp_cost": 6,
         "power": 1.5,
         "description": "小さな火球を放つ。威力1.5倍。",
         "unlock_distance": 1000
@@ -2377,7 +2377,7 @@ SKILLS_DATABASE = {
         "id": "軽傷治癒",
         "name": "軽傷治癒",
         "type": "heal",
-        "mp_cost": 20,
+        "mp_cost": 10,
         "heal_amount": 40,
         "description": "軽い傷を癒す。HP40回復。",
         "unlock_distance": 2000
@@ -2386,7 +2386,7 @@ SKILLS_DATABASE = {
         "id": "強攻撃",
         "name": "強攻撃",
         "type": "attack",
-        "mp_cost": 25,
+        "mp_cost": 10,
         "power": 1.8,
         "description": "強力な一撃。威力1.8倍。",
         "unlock_distance": 3000
@@ -2395,34 +2395,34 @@ SKILLS_DATABASE = {
         "id": "ファイアボール",
         "name": "ファイアボール",
         "type": "attack",
-        "mp_cost": 30,
+        "mp_cost": 15,
         "power": 2.2,
         "description": "炎の球を放つ。威力2.2倍。",
         "unlock_distance": 4000
-    },
-    "中治癒": {
-        "id": "中治癒",
-        "name": "中治癒",
-        "type": "heal",
-        "mp_cost": 35,
-        "heal_amount": 80,
-        "description": "傷を治す。HP80回復。",
-        "unlock_distance": 5000
     },
     "猛攻撃": {
         "id": "猛攻撃",
         "name": "猛攻撃",
         "type": "attack",
-        "mp_cost": 40,
+        "mp_cost": 20,
         "power": 2.5,
         "description": "猛烈な攻撃。威力2.5倍。",
+        "unlock_distance": 5000
+    },
+    "中治癒": {
+        "id": "中治癒",
+        "name": "中治癒",
+        "type": "heal",
+        "mp_cost": 20,
+        "heal_amount": 80,
+        "description": "傷を治す。HP80回復。",
         "unlock_distance": 6000
     },
     "爆炎": {
         "id": "爆炎",
         "name": "爆炎",
         "type": "attack",
-        "mp_cost": 45,
+        "mp_cost": 25,
         "power": 3.0,
         "description": "爆発する炎。威力3.0倍。",
         "unlock_distance": 7000
@@ -2431,7 +2431,7 @@ SKILLS_DATABASE = {
         "id": "完全治癒",
         "name": "完全治癒",
         "type": "heal",
-        "mp_cost": 50,
+        "mp_cost": 30,
         "heal_amount": 150,
         "description": "完全に傷を癒す。HP150回復。",
         "unlock_distance": 8000
@@ -2440,7 +2440,7 @@ SKILLS_DATABASE = {
         "id": "神速の一閃",
         "name": "神速の一閃",
         "type": "attack",
-        "mp_cost": 55,
+        "mp_cost": 35,
         "power": 3.5,
         "description": "神速の斬撃。威力3.5倍。",
         "unlock_distance": 9000
@@ -2449,7 +2449,7 @@ SKILLS_DATABASE = {
         "id": "究極魔法",
         "name": "究極魔法",
         "type": "attack",
-        "mp_cost": 60,
+        "mp_cost": 40,
         "power": 4.0,
         "description": "究極の魔法攻撃。威力4.0倍。",
         "unlock_distance": 10000
